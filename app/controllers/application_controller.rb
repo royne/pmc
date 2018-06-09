be4f2fca93ad60167906bfa73184d36c9e9b5e7f
royne <royne.chaves@gmail.com>
2018-06-09 14:20:33 -0500

    nuevo path al iniciar sesion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index, :show]
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def  after_sign_in_path_for ( students )
+    students_path
   end
 
 end
