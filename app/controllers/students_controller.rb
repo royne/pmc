bf4e10503795410e6d13be4e73bb38923c7a64b2
Royne Chaves <royne.chaves@gmail.com>
2018-06-08 19:34:33 -0500

    add panel admin

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,24 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = current_user.students.create(students_params)
+    if @student.save
+      redirect_to students_path, notice:"el estudiante se creo con exito"
+    else
+      flash[:alert] = @student.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+    def students_params
+      params.require(:student).permit(:name, :last_name, :cellphone, :age, :address, :course, :legal_guardian, :phone_lg, :eps)
+    end
 end
