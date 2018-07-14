2ff158cfcdcb3006c6ca91e944d61c653895eb5f
royne <royne.chaves@gmail.com>
2018-07-14 18:16:33 -0500

    funcionalidad estado ok

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -35,7 +35,14 @@ class StudentsController < ApplicationController
        @sum = @sum + payment.price
      end
     end
+  end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.update(params[:id], students_params)
   end
 
   private
