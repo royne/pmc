64689a26cb23f92303a73415c4760bd9b9265005
royne <royne.chaves@gmail.com>
2018-12-10 23:20:53 -0500

    agrega rol admin y nav lateral

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ after_create :assign_default_role
+
+ def assign_default_role
+   self.add_role(:user) if self.roles.blank?
+ end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  User.find(1).students.create(name: Faker::Name.name,last_name:Faker::Name.last_name,cellphone:Faker::PhoneNumber.cell_phone,email:"an@mail.com",age:28,address:Faker::Address.street_name,course:"sistemas",legal_guardian:Faker::Simpsons.character,phone_lg:Faker::PhoneNumber.cell_phone,eps:"abc",state:true)
+end
+
+5.times do
+  User.find(1).students.create(name: Faker::Name.name,last_name:Faker::Name.last_name,cellphone:Faker::PhoneNumber.cell_phone,email:"an@mail.com",age:28,address:Faker::Address.street_name,course:"full stack",legal_guardian:Faker::Simpsons.character,phone_lg:Faker::PhoneNumber.cell_phone,eps:"abc",state:true)
+end
+
+5.times do
+  User.find(1).students.create(name: Faker::Name.name,last_name:Faker::Name.last_name,cellphone:Faker::PhoneNumber.cell_phone,email:"an@mail.com",age:28,address:Faker::Address.street_name,course:"peluqueria",legal_guardian:Faker::Simpsons.character,phone_lg:Faker::PhoneNumber.cell_phone,eps:"abc",state:true)
+end
