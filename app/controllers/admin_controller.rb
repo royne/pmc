abf94b95c2fdeade41158329035a4ce40c3eafe5
royne <royne.chaves@gmail.com>
2018-12-12 22:57:52 -0500

    agrega modulo index de admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,12 @@
 class AdminController < ApplicationController
+  before_action :authenticate_admin
+
   def index
-    @users = User.all
+    @users = User.includes(:students).with_role('user')
+  end
+
+  private
+  def authenticate_admin
+    redirect_to(students_path) unless current_user.has_role? :admin
   end
 end
