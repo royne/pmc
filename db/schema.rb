9ec6bd809d0d90d26d1effd6eaa4db8247bf9933
Royne Chaves <royne.chaves@gmail.com>
2019-02-08 11:38:33 -0500

    agrega campos a usuarios y estado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190105014445) do
+ActiveRecord::Schema.define(version: 20190208154305) do
 
   create_table "courses", force: :cascade do |t|
     t.integer "user_id"
@@ -81,6 +81,10 @@ ActiveRecord::Schema.define(version: 20190105014445) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.boolean "state", default: true
+    t.string "name"
+    t.string "phone"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
