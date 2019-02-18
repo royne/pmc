c302e53a4d25dd0eaf58e85d92c947719f97dfed
royne <royne.chaves@gmail.com>
2019-02-17 20:41:48 -0500

    agrega vista de costos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
   def index
     render layout: false
   end
+
+  def costs
+    render layout: false
+  end
+
+  def about
+    render layout: false
+  end
 end
